6d7956f601bef5ea0ff5aad284e78c6aa89b9dad
Knowlester <sml.gw.ld@gmail.com>
2024-06-17 18:30:17 +0200

    pavé serpent maj cercles

--- pâté_au_serpent_ledupin.c
+++ pâté_au_serpent_ledupin.c
@@ -1,6 +1,7 @@
 #include <SDL2/SDL.h>
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #define M_PI 3.14159265358979323846
@@ -60,18 +61,18 @@ void resetbackground(SDL_Renderer *renderer){
     SDL_RenderFillRect(renderer, &rectangle);
 }
 
-void affichecercle(SDL_Renderer * renderer, float angle, int k, int j){
+void affichecercle(SDL_Renderer * renderer, float angle, int k, int j,int x,int y){
     SDL_SetRenderDrawColor(renderer,
                         (cos(angle * 2) + 1) * 255 / 2, // quantité de Rouge
                         (cos(angle * 5) + 1) * 255 / 2, //          de vert
                         (cos(angle) + 1) * 255 / 2,     //          de bleu
                         255);                           // opacité = opaque
     SDL_RenderDrawPoint(renderer,
-                        200 + (175 + k + j) * cos(angle),  // coordonnée en x     200 + (100 + k + j) * cos(angle)
-                        200 + (25 + k + j) * sin(angle)); //            en y     200 + (150 + k + j) * sin(angle)
+                        x + (175 + k + j) * cos(angle),  // coordonnée en x     200 + (100 + k + j) * cos(angle)
+                        y + (25 + k + j) * sin(angle)); //            en y     200 + (150 + k + j) * sin(angle)
 }
 
-void draw(SDL_Renderer * renderer, int k)
+void draw(SDL_Renderer * renderer, int k, int x, int y)
 {
 
     resetbackground(renderer);
@@ -81,15 +82,15 @@ void draw(SDL_Renderer * renderer, int k)
         for (int j = 0; j < 3; j++)
         {
             
-            affichecercle(renderer, angle,k,j );
+            affichecercle(renderer, angle,k,j , x ,y);
 
             if (k>20){
-                affichecercle(renderer, angle,k-20,j );
+                affichecercle(renderer, angle,k-20,j,x,y );
 
             }
 
             if (k>35){
-                affichecercle(renderer, angle,k-35,j );
+                affichecercle(renderer, angle,k-35,j,x,y );
 
             }
 
@@ -101,6 +102,10 @@ void draw(SDL_Renderer * renderer, int k)
 int main()
 {
     srand(time(0));
+    int lancement=0;
+
+    int mouseY;
+    int mouseX;
 
     SDL_Window *window = NULL;
     SDL_Renderer *renderer = NULL;
@@ -136,16 +141,52 @@ int main()
     /*********************************************************************************************************************/
     /*             Cette partie pourrait avantageusement être remplacée par la boucle évènementielle                     */
 
-    for (int k = 0; k < 50; k++)
-    {
-        draw(renderer, k);           // appel de la fonction qui crée l'image
+    SDL_bool program_on = SDL_TRUE;               // Booléen pour dire que le programme doit continuer
+    SDL_Event event;                              // c'est le type IMPORTANT !!
 
-        SDL_RenderPresent(renderer); // affichage
-        
-        SDL_Delay(100);               // Pause exprimée en ms
+    while (program_on){                           // Voilà la boucle des évènements 
 
 
-    }
+      if (SDL_PollEvent(&event)){                 // si la file d'évènements n'est pas vide : défiler l'élément en tête
+            
+                                                  // de file dans 'event'
+        switch(event.type){                       // En fonction de la valeur du type de cet évènement
+            case SDL_QUIT :                           // Un évènement simple, on a cliqué sur la x de la fenêtre
+            program_on = SDL_FALSE;                 // Il est temps d'arrêter le programme
+            break;
+            case SDL_MOUSEMOTION:                     // Click souris   
+
+            if (SDL_GetMouseState(NULL, NULL) & 
+                (SDL_BUTTON(SDL_BUTTON_LEFT) || SDL_BUTTON(SDL_BUTTON_RIGHT)) ) {         // Si c'est un click gauche
+                
+
+                lancement = 1;
+            
+            }
+
+                mouseX = event.motion.x;
+                mouseY = event.motion.y;
+            break;
+            default:                                  // L'évènement défilé ne nous intéresse pas
+            break;
+        }
+
+            if (lancement){
+                for (int k = 0; k < 30; k++)    {
+                    draw(renderer, k, mouseX ,mouseY);           // appel de la fonction qui crée l'image
+
+                    SDL_RenderPresent(renderer); // affichage
+                    
+                    SDL_Delay(100);               // Pause exprimée en ms
+                }
+
+
+                
+            }
+      }
+      
+     }  
+    
     SDL_Delay(3000); // Pause exprimée en ms
 
     /* on referme proprement la SDL */

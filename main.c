366bccf6e3b3dd05f9f5b64424680e274c7c28a8
Lucas Balmès <lucas.balmes@etu.uca.fr>
2024-06-17 13:43:02 +0200

    Création d'un mandala qui se rétracte

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
+#include <stdbool.h>
 
-/*****************************************/
-/*  exemple de création de fenêtres en X */
-/*****************************************/
+/********************************************/
+/*  exemple de création de fenêtres en roue */
+/********************************************/
 
 int main(int argc, char **argv)
 {
     (void)argc;
     (void)argv;
 
-    // Nombre de fenêtre qui tournent
-    int n = 6;
+    int n = 12; // Nombre de fenêtre qui tournent
     SDL_Window **window_tab = malloc(n * sizeof(SDL_Window *));
 
     /* Initialisation de la SDL  + gestion de l'échec possible */
@@ -22,14 +22,22 @@ int main(int argc, char **argv)
         exit(EXIT_FAILURE);
     }
 
+    SDL_DisplayMode DM;
+    SDL_GetCurrentDisplayMode(0, &DM);
+
+    int centerX = DM.w/2;
+    int centerY = DM.h/2;
+
+    int window_size = 200;
+
     for (int i = 0; i < n; i++)
     {
-        /* Création de la fenêtre de gauche */
+        /* Création de la fenêtre */
         window_tab[i] = SDL_CreateWindow(
-            "COUCOU",    // codage en utf8, donc accents possibles
-            0 + 100*i, 0 + 100*i,                  // coin haut gauche en haut gauche de l'écran
-            300, 300,              // largeur = 400, hauteur = 300
-            SDL_WINDOW_RESIZABLE); // redimensionnable
+            "COUCOU",                 // codage en utf8, donc accents possibles
+            0, 0,                     // coin haut gauche en haut gauche de l'écran
+            window_size, window_size, // largeur = 400, hauteur = 300
+            SDL_WINDOW_RESIZABLE);    // redimensionnable
 
         if (window_tab[i] == NULL)
         {
@@ -40,11 +48,26 @@ int main(int argc, char **argv)
         }
     }
 
-    /* Normalement, on devrait ici remplir les fenêtres... */
-    SDL_Delay(15000); // Pause exprimée  en ms
+    float const max_radius = 300.0;
+    for (int j = 0; j < 200; j++)
+    {
+        float rad = (2 * 3.14 / n);
+        for (int i = 0; i < n; i++)
+        {
+            int x = max_radius*(1.0 - fabs(sin(10 * rad + j * 0.1))) * sin(i * rad + j * 0.1) + centerX - window_size/2;
+            int y = max_radius*(1.0 - fabs(sin(10 * rad + j * 0.1))) * cos(i * rad + j * 0.1) + centerY - window_size/2;
+            SDL_SetWindowPosition(window_tab[i], x, y);
+            SDL_Delay(3); // Pause exprimée  en ms
+        }
+    }
 
     /* et on referme tout ce qu'on a ouvert en ordre inverse de la création */
-    SDL_DestroyWindow(window_1); // la fenêtre 1
+    for (int i = 0; i < n; i++)
+    {
+        SDL_DestroyWindow(window_tab[i]); // la fenêtre i
+    }
+
+    free(window_tab);
 
     SDL_Quit(); // la SDL
 

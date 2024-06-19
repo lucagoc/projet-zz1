13d4180398cb76fd5152443f3dc840f3a76197ad
Lucas Balmès <lucas.balmes@etu.uca.fr>
2024-06-19 09:26:50 +0200

    [MinMax] Ajout d'une fonction de copie de plateau

--- src/opponent.c
+++ src/opponent.c
@@ -1,1 +1,30 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 // Principalement là où sera utilisé min-max pour calculer le prochain coup.
+
+/**
+ * @brief Copie un plateau
+ * 
+ * @param board Plateau à copier
+ * @return int* Plateau copié
+ */
+int** copy_board(int board[6][6])
+{
+    int** new_board = malloc(6 * sizeof(int *));
+    for (int i = 0; i < 6; i++)
+    {
+        new_board[i] = malloc(6 * sizeof(int));
+    }
+    
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = 0; j < 6; j++)
+        {
+            new_board[i][j] = board[i][j];
+        }
+    }
+    return new_board;
+}
+

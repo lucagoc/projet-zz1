98bcacb1c54345ce73d40b57083b2e63a6439f2a
Knowlester <sml.gw.ld@gmail.com>
2024-06-19 16:52:22 +0200

    réparation erreur rules

--- src/rules.c
+++ src/rules.c
@@ -66,7 +66,6 @@ void initialise_pieces(int pieces_position[GRID_SIZE][GRID_SIZE], int king_black
         {
             pieces_position[i][j] = 0;
         }
-        
     }
 
     // ronin noir 1 ronin blanc 2 daimyo noir 3 daimyo blanc 4
@@ -155,9 +154,12 @@ void initialise_pieces(int pieces_position[GRID_SIZE][GRID_SIZE], int king_black
  */
 bool is_valid_move(int valid, int number_moves, int pieces_position[GRID_SIZE][GRID_SIZE], int xinit, int yinit, int xprev, int yprev, int xdesti, int ydesti)
 {
-    if (xinit<0 || yinit<0 ||  xinit>5 || yinit>5 ){
+    if (xinit < 0 || yinit < 0 || xinit > 5 || yinit > 5)
+    {
         return 0;
-    } else {
+    }
+    else
+    {
         if (number_moves == 0)
         {
             // plus aucun mouvement à effectuer
@@ -170,26 +172,26 @@ bool is_valid_move(int valid, int number_moves, int pieces_position[GRID_SIZE][G
         {
             // 1 ou plusieurs mouvements restants
 
-            if (xinit<5 && (xinit + 1 != xprev && yinit != yprev) && pieces_position[xinit + 1][yinit] == 0)
+            if (xinit < 5 && (xinit + 1 != xprev && yinit != yprev) && pieces_position[xinit + 1][yinit] == 0)
             {
 
                 valid = is_valid_move(0, number_moves - 1, pieces_position, xinit + 1, yinit, xinit, yinit, xdesti, ydesti);
 
                 // valid = 1 si on arrive sur la bonne case
             }
-            if (xinit>0 && valid == 0 && (xinit - 1 != xprev && yinit != yprev) && pieces_position[xinit - 1][yinit] == 0)
+            if (xinit > 0 && valid == 0 && (xinit - 1 != xprev && yinit != yprev) && pieces_position[xinit - 1][yinit] == 0)
             {
                 valid = is_valid_move(0, number_moves - 1, pieces_position, xinit - 1, yinit, xinit, yinit, xdesti, ydesti);
 
                 // valid = 1 si on arrive sur la bonne case
             }
-            if (yinit<5 && valid == 0 && (xinit != xprev && yinit + 1 != yprev) && pieces_position[xinit][yinit + 1] == 0)
+            if (yinit < 5 && valid == 0 && (xinit != xprev && yinit + 1 != yprev) && pieces_position[xinit][yinit + 1] == 0)
             {
                 valid = is_valid_move(0, number_moves - 1, pieces_position, xinit + 1, yinit + 1, xinit, yinit, xdesti, ydesti);
 
                 // valid = 1 si on arrive sur la bonne case
             }
-            if (xinit>0 && valid == 0 && (xinit != xprev && yinit - 1 != yprev) && pieces_position[xinit][yinit - 1] == 0)
+            if (xinit > 0 && valid == 0 && (xinit != xprev && yinit - 1 != yprev) && pieces_position[xinit][yinit - 1] == 0)
             {
                 valid = is_valid_move(0, number_moves - 1, pieces_position, xinit + 1, yinit - 1, xinit, yinit, xdesti, ydesti);
 
@@ -197,7 +199,6 @@ bool is_valid_move(int valid, int number_moves, int pieces_position[GRID_SIZE][G
             }
             return valid;
         }
-
     }
 }
 
@@ -222,11 +223,13 @@ bool is_valid_move(int valid, int number_moves, int pieces_position[GRID_SIZE][G
 bool can_play(int player, int possible_move, int number_moves, int position_piece[GRID_SIZE][GRID_SIZE], int xinit, int yinit, int xprev, int yprev)
 {
 
-
-    if (xinit<0 || yinit<0 ||  xinit>5 || yinit>5 ){
+    if (xinit < 0 || yinit < 0 || xinit > 5 || yinit > 5)
+    {
         return 0;
-    } else {
-            
+    }
+    else
+    {
+
         if (number_moves == 0)
         {
             // plus aucun mouvement à effectuer
@@ -242,27 +245,27 @@ bool can_play(int player, int possible_move, int number_moves, int position_piec
                 {
 
                     // si on ne revient pas sur la case prec, si la case à droite est libre
-                    if (xinit<5 && (xinit + 1 != xprev && yinit != yprev) && (position_piece[xinit + 1][yinit] == 0 || position_piece[xinit + 1][yinit] == 2 || position_piece[xinit + 1][yinit] == 4))
+                    if (xinit < 5 && (xinit + 1 != xprev && yinit != yprev) && (position_piece[xinit + 1][yinit] == 0 || position_piece[xinit + 1][yinit] == 2 || position_piece[xinit + 1][yinit] == 4))
                     {
 
                         possible_move = 1;
 
                         // valid = 1 si on arrive sur la bonne case
                     }
-                    if (xinit>0 && possible_move == 0 && (xinit - 1 != xprev && yinit != yprev) && (position_piece[xinit - 1][yinit] == 0 || position_piece[xinit - 1][yinit] == 2 || position_piece[xinit - 1][yinit] == 4))
+                    if (xinit > 0 && possible_move == 0 && (xinit - 1 != xprev && yinit != yprev) && (position_piece[xinit - 1][yinit] == 0 || position_piece[xinit - 1][yinit] == 2 || position_piece[xinit - 1][yinit] == 4))
                     {
                         possible_move = 1;
 
                         // possible_move = 1 si on arrive sur la bonne case
                     }
-                    if (yinit<5 && possible_move == 0 && (xinit != xprev && yinit + 1 != yprev) && (position_piece[xinit][yinit + 1] == 0 || position_piece[xinit][yinit + 1] == 2 || position_piece[xinit][yinit + 1] == 4))
+                    if (yinit < 5 && possible_move == 0 && (xinit != xprev && yinit + 1 != yprev) && (position_piece[xinit][yinit + 1] == 0 || position_piece[xinit][yinit + 1] == 2 || position_piece[xinit][yinit + 1] == 4))
                     {
                         possible_move = 1;
 
                         // valid = 1 si on arrive sur la bonne case
                     }
 
-                    if (yinit>0 && possible_move == 0 && (xinit != xprev && yinit - 1 != yprev) && (position_piece[xinit][yinit - 1] == 0 || position_piece[xinit][yinit - 1] == 2 || position_piece[xinit][yinit - 1] == 4))
+                    if (yinit > 0 && possible_move == 0 && (xinit != xprev && yinit - 1 != yprev) && (position_piece[xinit][yinit - 1] == 0 || position_piece[xinit][yinit - 1] == 2 || position_piece[xinit][yinit - 1] == 4))
                     {
                         possible_move = 1;
 
@@ -273,27 +276,27 @@ bool can_play(int player, int possible_move, int number_moves, int position_piec
                 {
 
                     // si on ne revient pas sur la case prec, si la case à droite est libre
-                    if (xinit<5 && (xinit + 1 != xprev && yinit != yprev) && (position_piece[xinit + 1][yinit] == 0 || position_piece[xinit + 1][yinit] == 1 || position_piece[xinit + 1][yinit] == 3))
+                    if (xinit < 5 && (xinit + 1 != xprev && yinit != yprev) && (position_piece[xinit + 1][yinit] == 0 || position_piece[xinit + 1][yinit] == 1 || position_piece[xinit + 1][yinit] == 3))
                     {
 
                         possible_move = 1;
 
                         // valid = 1 si on arrive sur la bonne case
                     }
-                    if (xinit>0 && possible_move == 0 && (xinit - 1 != xprev && yinit != yprev) && (position_piece[xinit - 1][yinit] == 0 || position_piece[xinit - 1][yinit] == 1 || position_piece[xinit - 1][yinit] == 3))
+                    if (xinit > 0 && possible_move == 0 && (xinit - 1 != xprev && yinit != yprev) && (position_piece[xinit - 1][yinit] == 0 || position_piece[xinit - 1][yinit] == 1 || position_piece[xinit - 1][yinit] == 3))
                     {
                         possible_move = 1;
 
                         // possible_move = 1 si on arrive sur la bonne case
                     }
-                    if (yinit<5 && possible_move == 0 && (xinit != xprev && yinit + 1 != yprev) && (position_piece[xinit][yinit + 1] == 0 || position_piece[xinit][yinit + 1] == 1 || position_piece[xinit][yinit + 1] == 3))
+                    if (yinit < 5 && possible_move == 0 && (xinit != xprev && yinit + 1 != yprev) && (position_piece[xinit][yinit + 1] == 0 || position_piece[xinit][yinit + 1] == 1 || position_piece[xinit][yinit + 1] == 3))
                     {
                         possible_move = 1;
 
                         // valid = 1 si on arrive sur la bonne case
                     }
 
-                    if (yinit>0 && possible_move == 0 && (xinit != xprev && yinit - 1 != yprev) && (position_piece[xinit][yinit - 1] == 0 || position_piece[xinit][yinit - 1] == 1 || position_piece[xinit][yinit - 1] == 3))
+                    if (yinit > 0 && possible_move == 0 && (xinit != xprev && yinit - 1 != yprev) && (position_piece[xinit][yinit - 1] == 0 || position_piece[xinit][yinit - 1] == 1 || position_piece[xinit][yinit - 1] == 3))
                     {
                         possible_move = 1;
 
@@ -306,27 +309,27 @@ bool can_play(int player, int possible_move, int number_moves, int position_piec
             {
 
                 // si on ne revient pas sur la case prec, si la case à droite est libre
-                if (xinit<5 && (xinit + 1 != xprev && yinit != yprev) && position_piece[xinit + 1][yinit] == 0)
+                if (xinit < 5 && (xinit + 1 != xprev && yinit != yprev) && position_piece[xinit + 1][yinit] == 0)
                 {
 
                     possible_move = can_play(player, 0, number_moves - 1, position_piece, xinit + 1, yinit, xinit, yinit);
 
                     // valid = 1 si on arrive sur la bonne case
                 }
-                if (xinit>0 && possible_move == 0 && (xinit - 1 != xprev && yinit != yprev) && position_piece[xinit - 1][yinit] == 0)
+                if (xinit > 0 && possible_move == 0 && (xinit - 1 != xprev && yinit != yprev) && position_piece[xinit - 1][yinit] == 0)
                 {
                     possible_move = can_play(player, 0, number_moves - 1, position_piece, xinit - 1, yinit, xinit, yinit);
 
                     // possible_move = 1 si on arrive sur la bonne case
                 }
-                if (yinit<5 && possible_move == 0 && (xinit != xprev && yinit + 1 != yprev) && position_piece[xinit][yinit + 1] == 0)
+                if (yinit < 5 && possible_move == 0 && (xinit != xprev && yinit + 1 != yprev) && position_piece[xinit][yinit + 1] == 0)
                 {
                     possible_move = can_play(player, 0, number_moves - 1, position_piece, xinit + 1, yinit + 1, xinit, yinit);
 
                     // valid = 1 si on arrive sur la bonne case
                 }
 
-                if (yinit>0 && possible_move == 0 && (xinit != xprev && yinit - 1 != yprev) && position_piece[xinit][yinit - 1] == 0)
+                if (yinit > 0 && possible_move == 0 && (xinit != xprev && yinit - 1 != yprev) && position_piece[xinit][yinit - 1] == 0)
                 {
                     possible_move = can_play(player, 0, number_moves - 1, position_piece, xinit + 1, yinit - 1, xinit, yinit);
 
@@ -338,114 +341,115 @@ bool can_play(int player, int possible_move, int number_moves, int position_piec
     }
 }
 
-bool valid_bird_placement(int xinit, int yinit, int birdX, int birdY, int pieces_position[GRID_SIZE][GRID_SIZE], int board[GRID_SIZE][GRID_SIZE]){
-    
-    return ((pieces_position[birdX][birdY]==0 || pieces_position[birdX][birdY]==5) && (board[birdX][birdY]==board[xinit][yinit])){
+bool valid_bird_placement(int xinit, int yinit, int birdX, int birdY, int pieces_position[GRID_SIZE][GRID_SIZE], int board[GRID_SIZE][GRID_SIZE])
+{
+
+    return ((pieces_position[birdX][birdY] == 0 || pieces_position[birdX][birdY] == 5) && (board[birdX][birdY] == board[xinit][yinit]));
 
 }
 
-/* Vérifie si le joueur peut encore jouer */
+    /* Vérifie si le joueur peut encore jouer */
 
-int number_moves_to_play(int board[6][6], int xinit, int yinit)
-{
-    return board[xinit][yinit];
-}
-
-/**
- * @brief Joue le coup indiqué par le joueur et retourne 1 si c'est possible, retourne 0 sinon
- *
- * @param player 0 si on est sur le joueur noir ou 1 si le joueur blanc
- * @param victory contient l'adresse vers une case contenant: 0 si pas de victoire, 1 si victoire noire, 2 si victoire blanche
- *
- * @param captured_black_piece contient l'adresse vers un compteur du nombre de ronin noirs capturés
- * @param captured_white_piece contient l'adresse vers un compteur du nombre de ronin blancs capturés
- *
- * @param pieces_position tableau qui contient les pièces sur le plateau: 0 aucun 1 ronin noir 2 ronin blanc 3 daimyo noir 4 daimyo blanc 5 le piaf
- * @param board contient les numéros des cases de la grille
- *
- * @param xinit position de la pièce jouée
- * @param yinit position de la pièce jouée
- *
- * @param xdesti position du coup joué
- * @param ydesti position du coup joué
- *
- *@param advantage 0 si le coup dépend de l'oiseau, 1 sinon (cas où le coup précédent est impossible)
- *
- */
-bool playing_move(int player, int *victory, int *captured_black_piece, int *captured_white_piece, int pieces_position[GRID_SIZE][GRID_SIZE], int board[6][6], int xinit, int yinit, int xdesti, int ydesti)
-{
-
-    int valid_move_played = 0;
-    if (is_valid_move(0, board[xinit][yinit], pieces_position, xinit, yinit, xinit, yinit, xdesti, ydesti))
+    int number_moves_to_play(int board[6][6], int xinit, int yinit)
     {
-        valid_move_played = 1;
+        return board[xinit][yinit];
+    }
+
+    /**
+     * @brief Joue le coup indiqué par le joueur et retourne 1 si c'est possible, retourne 0 sinon
+     *
+     * @param player 0 si on est sur le joueur noir ou 1 si le joueur blanc
+     * @param victory contient l'adresse vers une case contenant: 0 si pas de victoire, 1 si victoire noire, 2 si victoire blanche
+     *
+     * @param captured_black_piece contient l'adresse vers un compteur du nombre de ronin noirs capturés
+     * @param captured_white_piece contient l'adresse vers un compteur du nombre de ronin blancs capturés
+     *
+     * @param pieces_position tableau qui contient les pièces sur le plateau: 0 aucun 1 ronin noir 2 ronin blanc 3 daimyo noir 4 daimyo blanc 5 le piaf
+     * @param board contient les numéros des cases de la grille
+     *
+     * @param xinit position de la pièce jouée
+     * @param yinit position de la pièce jouée
+     *
+     * @param xdesti position du coup joué
+     * @param ydesti position du coup joué
+     *
+     *@param advantage 0 si le coup dépend de l'oiseau, 1 sinon (cas où le coup précédent est impossible)
+     *
+     */
+    bool playing_move(int player, int *victory, int *captured_black_piece, int *captured_white_piece, int pieces_position[GRID_SIZE][GRID_SIZE], int board[6][6], int xinit, int yinit, int xdesti, int ydesti)
+    {
+
+        int valid_move_played = 0;
+        if (is_valid_move(0, board[xinit][yinit], pieces_position, xinit, yinit, xinit, yinit, xdesti, ydesti))
+        {
+            valid_move_played = 1;
+            if (player == 0)
+            {
+
+                if (pieces_position[xdesti][ydesti] == 2)
+                {
+                    *captured_white_piece += 1;
+                }
+                else if (pieces_position[xdesti][ydesti] == 4)
+                {
+                    *victory = 1;
+                }
+            }
+            else
+            {
+                if (pieces_position[xdesti][ydesti] == 1)
+                {
+                    *captured_black_piece += 1;
+                }
+                else if (pieces_position[xdesti][ydesti] == 3)
+                {
+                    *victory = 2;
+                }
+            }
+
+            pieces_position[xdesti][ydesti] = pieces_position[xinit][yinit];
+            pieces_position[xinit][yinit] = 0;
+        }
+        return valid_move_played;
+    }
+
+    /**
+     * @brief Respawne une pièce à l'endroit indiqué et renvoie 1 si c'est possible, renvoie 0 sinon
+     *
+     * @param player 0 si on est sur le joueur noir ou 1 si le joueur blanc
+
+     * @param captured_white_piece nombre de pièces blanches capturées
+
+     * @param captured_black_piece nombre de pièces noires capturées
+
+     * @param pieces_position tableau qui contient les pièces sur le plateau: 0 aucun 1 ronin noir 2 ronin blanc 3 daimyo noir 4 daimyo blanc 5 le piaf
+      *
+      * @param xres position de la pièce replacée
+      * @param yres position de la pièce replacée
+      *
+     */
+
+    bool respawn_piece(int player, int captured_white_piece, int captured_black_piece, int pieces_position[GRID_SIZE][GRID_SIZE], int xres, int yres)
+    {
+
+        int respawn_possible = 0;
+
         if (player == 0)
         {
-
-            if (pieces_position[xdesti][ydesti] == 2)
+            if (captured_black_piece > 0 && pieces_position[xres][yres] == 0)
             {
-                *captured_white_piece+=1;
-            }
-            else if (pieces_position[xdesti][ydesti] == 4)
-            {
-                *victory = 1;
+                respawn_possible = 1;
+                pieces_position[xres][yres] = 1; // on place un ronin noir
             }
         }
         else
         {
-            if (pieces_position[xdesti][ydesti] == 1)
+            if (captured_white_piece > 0 && pieces_position[xres][yres] == 0)
             {
-                *captured_black_piece+=1;
-            }
-            else if (pieces_position[xdesti][ydesti] == 3)
-            {
-                *victory = 2;
+                respawn_possible = 1;
+                pieces_position[xres][yres] = 2; // on place un ronin blanc
             }
         }
 
-        pieces_position[xdesti][ydesti] = pieces_position[xinit][yinit];
-        pieces_position[xinit][yinit] = 0;
+        return respawn_possible;
     }
-    return valid_move_played;
-}
-
-/**
- * @brief Respawne une pièce à l'endroit indiqué et renvoie 1 si c'est possible, renvoie 0 sinon
- *
- * @param player 0 si on est sur le joueur noir ou 1 si le joueur blanc
-
- * @param captured_white_piece nombre de pièces blanches capturées
-
- * @param captured_black_piece nombre de pièces noires capturées
-
- * @param pieces_position tableau qui contient les pièces sur le plateau: 0 aucun 1 ronin noir 2 ronin blanc 3 daimyo noir 4 daimyo blanc 5 le piaf
-  *
-  * @param xres position de la pièce replacée
-  * @param yres position de la pièce replacée
-  *
- */
-
-bool respawn_piece(int player, int captured_white_piece, int captured_black_piece, int pieces_position[GRID_SIZE][GRID_SIZE], int xres, int yres)
-{
-
-    int respawn_possible = 0;
-
-    if (player == 0)
-    {
-        if (captured_black_piece > 0 && pieces_position[xres][yres] == 0)
-        {
-            respawn_possible = 1;
-            pieces_position[xres][yres] = 1; // on place un ronin noir
-        }
-    }
-    else
-    {
-        if (captured_white_piece > 0 && pieces_position[xres][yres] == 0)
-        {
-            respawn_possible = 1;
-            pieces_position[xres][yres] = 2; // on place un ronin blanc
-        }
-    }
-
-    return respawn_possible;
-}
